ff4c07a0cbb8cc94734493e77555568fe832959c
Jimmy García <jimmyegc@gmail.com>
2025-09-02 19:30:38 -0600

    09022025

---
+++ database/api_flask.sql
@@ -0,0 +1,80 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 03-09-2025 a las 03:29:14
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `api_flask`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `curso`
+--
+
+CREATE TABLE `curso` (
+  `codigo` char(6) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `creditos` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `curso`
+--
+
+INSERT INTO `curso` (`codigo`, `nombre`, `creditos`) VALUES
+('123', 'Programación Python', 5),
+('555', 'Temporal UPDATED!', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `task`
+--
+
+CREATE TABLE `task` (
+  `id` int(11) NOT NULL,
+  `title` varchar(70) NOT NULL,
+  `description` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `task`
+--
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `title` (`title`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `task`
+--
+ALTER TABLE `task`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from config import config
 from flask_mysqldb import MySQL
 
@@ -40,7 +40,49 @@ def leer_curso(codigo):
       return jsonify({ "status": "Ok", "message": "Curso no encontrado", "data": {} })
   except Exception as ex:
     return jsonify({ "status": "Error", "message": ex })
-  
+
+@app.route('/cursos', methods=['POST'])
+def crear_curso():
+  try:
+    # print(request.json)
+    cursor=conexion.connection.cursor()
+    sql = """INSERT INTO curso (codigo, nombre, creditos) 
+    VALUES ('{0}','{1}','{2}')""".format(
+      request.json['codigo'], request.json['nombre'], request.json['creditos']
+    )    
+    cursor.execute(sql)
+    conexion.connection.commit()
+    return jsonify({ "status": "Ok", "message": "Curso registrado" })
+  except Exception as ex:
+    return jsonify({ "status": "Error", "message": ex })
+
+@app.route('/cursos/<codigo>', methods=['DELETE'])
+def eliminar_curso(codigo):
+  try:
+    # print(request.json)
+    cursor=conexion.connection.cursor()
+    sql = "DELETE FROM curso WHERE codigo = '{0}'".format(codigo)
+    cursor.execute(sql)
+    conexion.connection.commit()
+    return jsonify({ "status": "Ok", "message": "Curso eliminado" })
+  except Exception as ex:
+    return jsonify({ "status": "Error", "message": ex })
+
+@app.route('/cursos/<codigo>', methods=['PUT'])
+def actualizar_curso(codigo):
+  try:
+    # print(request.json)
+    cursor=conexion.connection.cursor()
+    sql = """UPDATE curso SET nombre = '{0}', creditos = {1}
+    WHERE codigo = '{2}'""".format(
+      request.json['nombre'], request.json['creditos'], codigo
+    )    
+    cursor.execute(sql)
+    conexion.connection.commit()
+    return jsonify({ "status": "Ok", "message": "Curso actualizado" })
+  except Exception as ex:
+    return jsonify({ "status": "Error", "message": ex })
+
 def pagina_no_encontrada(error):
   return "<h1>Página no encontrada</h1>"
 
